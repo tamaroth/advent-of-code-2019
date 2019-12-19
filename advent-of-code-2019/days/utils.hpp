abefb7b53ce0e79d215f05274f13ba239697b1d6
tamaroth <tamaroth@vivaldi.net>
2019-12-19 14:54:09 +0100

    Add time measuring function.

--- advent-of-code-2019/days/utils.hpp
+++ advent-of-code-2019/days/utils.hpp
@@ -1,7 +1,33 @@
 #pragma once
 
+#include <chrono>
+#include <iostream>
 #include <string>
+#include <string_view>
+#include <sstream>
 #include <vector>
 
 std::string int_to_str(int value);
 std::vector<std::string> split_string_by(const std::string& src, const std::string& delimiter);
+
+using ms = std::chrono::milliseconds;
+using us = std::chrono::microseconds;
+
+template<typename Timepoint>
+std::string duration_to_string(Timepoint start, Timepoint end) {
+	std::ostringstream os;
+	if (auto elapsed = std::chrono::duration_cast<ms>(end-start); elapsed.count()) {
+		os << elapsed.count() << "ms.";
+	} else {
+		os << std::chrono::duration_cast<us>(end-start).count() << "µs.";;
+	}
+	return os.str();
+}
+
+template<typename Block, typename ...Args>
+void measure_execution_time(std::string_view task, Block block) {
+		auto start = std::chrono::high_resolution_clock::now();
+		block();
+		auto end = std::chrono::high_resolution_clock::now();
+		std::cout << task << "executed in " << duration_to_string(start, end) << std::endl;
+}
